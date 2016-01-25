1d95ff242573c8f5f66bde5f4bdaf52264530aec
geckofeetnyc <geckofeetnyc@gmail.com>
2016-01-25 14:06:50 -0500

    Update tube.js

--- js/tube.js
+++ js/tube.js
@@ -1,3 +1,4 @@
 $().ready(function() {
-                $('#tubular').tubular({videoId: 'eIr8MH20aUQ'}); // where idOfYourVideo is the YouTube ID.
+                $('#wrapper').tubular({videoId: 'eIr8MH20aUQ'}); // where idOfYourVideo is the YouTube ID.
         });
+ 
